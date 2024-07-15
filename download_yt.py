f06f32bbf6fa4c644b8895ae50e5811c988d68dc
liucheyu <liucheyu1987@gmail.com>
2024-07-15 17:11:50 +0800

    update

--- download_yt.py
+++ download_yt.py
@@ -1,15 +1,26 @@
 import yt_dlp
 import os
 
-savePath = './vedio'
+
+url='https://www.youtube.com/watch?v=hOGpFkEukjM&list=PLmOn9nNkQxJGuxM62QOkUwD_zroNZj-a7&index=2&pp=iAQB'
+save_dir = './vedio'
+
+    # 检查并创建目录
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 # 定义下载选项
 ydl_opts = {
-    'format': 'bestvideo/best',  # 下载最佳视频和音频质量
-    'outtmpl': os.path.join(savePath, '%(title)s.%(ext)s'),        # 下载后的文件命名格式
-    'progress_hooks': [lambda d: print(d['status']) if d['status'] == 'downloading' else None]
+    'format': 'bestvideo+bestaudio/best',  # 下载最高质量的视频和音频
+    'outtmpl': os.path.join(save_dir, '%(title)s.%(ext)s'),  # 指定保存目录和文件命名格式
+    #'merge_output_format': 'mp4',  # 合并视频和音频为mp4格式
+    'progress_hooks': [lambda d: print(d['status']) if d['status'] == 'downloading' else None],
+    # 'postprocessors': [{
+    #     'key': 'FFmpegVideoConvertor',
+    #     'preferedformat': 'mp4',  # 指定合并后的格式
+    # }],
 }
 
 # 下载视频
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=hOGpFkEukjM&list=PLmOn9nNkQxJGuxM62QOkUwD_zroNZj-a7&index=2&pp=iAQB'])
+    ydl.download([url])
--- readme.md
+++ readme.md
@@ -2,8 +2,19 @@
 
 [來源](https://github.com/yt-dlp/yt-dlp)
 
+可以直接下載下來執行，不需要透過python
+
+指令:
+```
+yt-dlp.exe -P "要儲存的目錄" -f "bestvideo+bestaudio/best" URL "要下載的url"
+```
+
+似乎有依賴ffmpeg，下載指定bestvideo+bestaudio/best不會合併時，要裝一下
+[ffmpeg](https://ffmpeg.org/download.html)
+
+
+要自行編寫的話，
 套件
 ```
 pip install yt-dlp
 ```
-
